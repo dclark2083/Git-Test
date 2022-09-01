0f5e4dccf3625e34199595f3ec44c330331ef558
Darren Clark <darclar@ut.utm.edu>
2022-09-01 16:22:17 -0500

    Update main.cpp to show git diff usage.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,26 @@
 
 using namespace std;
 
+int sum (int userNum);
+
 int main() {
 
-  cout << "Hello World." << endl;
+  cout << "Please enter a number: " << endl;
+  int n;
+  cin >> n;
+
+  cout << sum(n) << endl;
   
   return 0;
+}
+
+int sum (int userNum) {
+
+  int total = 0;
+
+  for (int i = 0; i < userNum; i++) {
+    total += userNum;
+  }
+
+  return total;
 }
